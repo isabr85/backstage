308ffb2879ab933f2bc8c36f6803c2a689e61ed9
Mike Lewis <mtlewis@users.noreply.github.com>
2021-08-05 14:58:00 +0100

    scaffolder: merge default field extensions with custom field extensions
    Signed-off-by: Mike Lewis <mtlewis@users.noreply.github.com>

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -45,10 +45,6 @@ import {
   ScaffolderPage,
   scaffolderPlugin,
   ScaffolderFieldExtensions,
-  RepoUrlPickerFieldExtension,
-  OwnerPickerFieldExtension,
-  EntityPickerFieldExtension,
-  EntityNamePickerFieldExtension,
 } from '@backstage/plugin-scaffolder';
 import { SearchPage } from '@backstage/plugin-search';
 import { TechRadarPage } from '@backstage/plugin-tech-radar';
@@ -119,10 +115,6 @@ const routes = (
     <Route path="/docs" element={<TechdocsPage />} />
     <Route path="/create" element={<ScaffolderPage />}>
       <ScaffolderFieldExtensions>
-        <EntityPickerFieldExtension />
-        <EntityNamePickerFieldExtension />
-        <RepoUrlPickerFieldExtension />
-        <OwnerPickerFieldExtension />
         <LowerCaseValuePickerFieldExtension />
       </ScaffolderFieldExtensions>
     </Route>
